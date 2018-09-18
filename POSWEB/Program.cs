74b019e8af360dcf984be1c6589a56c6aceb5327
Arthur Jardim <arthurjardim2@gmail.com>
2018-09-18 19:02:21 -0300

    Adicionar arquivos de projeto.

---
+++ POSWEB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POSWEB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
